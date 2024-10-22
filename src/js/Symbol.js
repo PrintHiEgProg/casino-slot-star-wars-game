443cf98ce1336755a86bcb2e2eff3c2784b4f55a
PrintHiEgProg <egorkobzev23gmail.com>
2024-10-22 12:16:18 +0300

    + input bet

--- src/js/Symbol.js
+++ src/js/Symbol.js
@@ -21,14 +21,14 @@ export default class Symbol {
   static get symbols() {
     return [
       "at_at",
-      "c3po",
-      "darth_vader",
-      "death_star",
-      "falcon",
-      "r2d2",
-      "stormtrooper",
-      "tie_ln",
-      "yoda",
+      //"c3po",
+      //"darth_vader",
+      //"death_star",
+      //"falcon",
+      //"r2d2",
+      //"stormtrooper",
+      //"tie_ln",
+      //"yoda",
     ];
   }
 
